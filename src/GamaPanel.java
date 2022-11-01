cd05aa40f313cdbdbdeed0f1254104b4821faea3
Evgeny Polyakov <evg_pol@yahoo.com>
2022-11-01 19:25:39 +0300

    added logic

--- src/GamaPanel.java
+++ src/GamaPanel.java
@@ -11,43 +11,138 @@ public class GamaPanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 75;
+    static final int DELAY = 100;
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
     int bodyParts = 3;
     int applesEaten;
-    int applesX;
-    int applesY;
+    int appleX;
+    int appleY;
     char direction = 'R';
     boolean running = false;
     Timer timer;
     Random random;
 
-    GamaPanel() {}
+    GamaPanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
 
-    public void startGame() {}
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
 
-    public void paintComponent(Graphics graphics) {}
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        draw(graphics);
+    }
 
-    public void draw(Graphics graphics) {}
+    public void draw(Graphics graphics) {
+        // draw grid
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            graphics.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            graphics.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
 
-    public void move() {}
+        // draw apple
+        graphics.setColor(Color.RED);
+        graphics.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-    public void checkApple() {}
+        // draw snake
+        for (int i = 0; i < bodyParts; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        }
+    }
 
-    public void checkCollisions() {}
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        switch (direction) {
+            case 'U' -> y[0] = y[0] - UNIT_SIZE;
+            case 'D' -> y[0] = y[0] + UNIT_SIZE;
+            case 'L' -> x[0] = x[0] - UNIT_SIZE;
+            case 'R' -> x[0] = x[0] + UNIT_SIZE;
+        }
+    }
+
+    public void newApple() {
+        appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+    }
+
+    public void checkApple() {
+        if(x[0] == appleX && y[0] == appleY) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    public void checkCollisions() {
+        // body
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+                break;
+            }
+        }
+
+        // left wall
+        if(x[0] < 0) running = false;
+        // right wall
+        if(x[0] > SCREEN_WIDTH) running = false;
+        // top wall
+        if(y[0] < 0) running = false;
+        // bottom wall
+        if(y[0] > SCREEN_HEIGHT) running = false;
+
+        if(!running) timer.stop();
+    }
 
     public void gameOver(Graphics graphics) {}
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
 
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
+           switch (e.getKeyCode()) {
+               case KeyEvent.VK_LEFT:
+                   if(direction != 'R') direction = 'L';
+                   break;
+
+               case KeyEvent.VK_RIGHT:
+                   if(direction != 'L') direction = 'R';
+                   break;
+
+               case KeyEvent.VK_UP:
+                   if(direction != 'D') direction = 'U';
+                   break;
+
+               case KeyEvent.VK_DOWN:
+                   if(direction != 'U') direction = 'D';
+                   break;
+           }
         }
     }
 }
